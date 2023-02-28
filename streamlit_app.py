3c76ff11a589bd6945af48c68e91b70ca5944434
benisha-rose <126146639+benisha-rose@users.noreply.github.com>
2023-02-28 18:47:42 +0530

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,3 @@ fruits_selected = streamlit.multiselect("pick some fruits :", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(my_fruit_list)
-
