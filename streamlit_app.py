943d5a3a5312793901dc516d1481d3ecf85a43a9
benisha-rose <126146639+benisha-rose@users.noreply.github.com>
2023-02-28 18:53:23 +0530

    Update streamlit_app.py
    request commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,7 @@ fruits_selected = streamlit.multiselect("pick some fruits :", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(my_fruit_list)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
